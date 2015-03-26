1a860c554ed4e206552cbe364ca0fac810a6401f
The Datalorax <datalorax@gmail.com>
2015-03-26 19:12:48 +0000

    Harden ChangeCollectionElementsMutator

--- populace-core/src/main/java/org/datalorax/populace/core/populate/mutator/change/ChangeCollectionElementsMutator.java
+++ populace-core/src/main/java/org/datalorax/populace/core/populate/mutator/change/ChangeCollectionElementsMutator.java
@@ -19,6 +19,7 @@ package org.datalorax.populace.core.populate.mutator.change;
 import org.apache.commons.lang3.Validate;
 import org.datalorax.populace.core.populate.Mutator;
 import org.datalorax.populace.core.populate.PopulatorContext;
+import org.datalorax.populace.core.populate.PopulatorException;
 import org.datalorax.populace.core.util.TypeUtils;
 
 import java.lang.reflect.Type;
@@ -27,11 +28,11 @@ import java.util.Collection;
 
 /**
  * Change mutator for {@link java.util.Collection collections}.
- *
+ * <p>
  * If the currentValue is null then this mutator does nothing. Consider using
  * {@link org.datalorax.populace.core.populate.mutator.ensure.EnsureMutator} to first ensure the current value is not null,
  * if the required behaviour is to always ensure a non-null, populated collection instance.
- *
+ * <p>
  * Non null values are modified by adding an additional instance to the collection of the required type.
  * This is done as mutating entries of some collection implementations, e.g. {@link java.util.Set}, can invalidate the
  * collection and lead to undefined behaviour.
@@ -42,6 +43,31 @@ public class ChangeCollectionElementsMutator implements Mutator {
     public static final ChangeCollectionElementsMutator INSTANCE = new ChangeCollectionElementsMutator();
     private static final TypeVariable<Class<Collection>> COLLECTION_TYPE_VARIABLE = Collection.class.getTypeParameters()[0];
 
+    @SuppressWarnings("unchecked")
+    private static Collection<Object> ensureCollection(final Type type, final Object currentValue) {
+        Validate.isAssignableFrom(Collection.class, TypeUtils.getRawType(type, null), "Unsupported type %s", type);
+        return (Collection<Object>) currentValue;
+    }
+
+    private static Type getActualComponentType(final Type type, final Collection<Object> collection) {
+        final Object element = findNonNullElement(collection);
+        if (element != null) {
+            return element.getClass();
+        }
+
+        return TypeUtils.getTypeArgument(type, Collection.class, COLLECTION_TYPE_VARIABLE);
+    }
+
+    private static Object findNonNullElement(final Collection<Object> collection) {
+        for (Object o : collection) {
+            if (o != null) {
+                return o;
+            }
+        }
+
+        return null;
+    }
+
     @Override
     public Collection<?> mutate(Type type, Object currentValue, final Object parent, PopulatorContext config) {
         final Collection<Object> collection = ensureCollection(type, currentValue);
@@ -49,26 +75,32 @@ public class ChangeCollectionElementsMutator implements Mutator {
             return null;
         }
 
-        final Type defaultComponentType = getComponentType(type);
-        final Object element = findNonNullElement(collection);
-        final Type componentType = element == null ? defaultComponentType : element.getClass();
-        final Mutator componentMutator = element == null ? config.getMutator(defaultComponentType) : config.getMutator(element.getClass());
+        final Type componentType = getActualComponentType(type, collection);
+        final Mutator componentMutator = config.getMutator(componentType);
 
         Object newItem = config.createInstance(componentType, null);
         boolean added;
         do {
-            newItem = componentMutator.mutate(componentType, newItem, null, config);
+            final Object mutated = componentMutator.mutate(componentType, newItem, null, config);
 
-            added = collection.add(newItem);
+            added = collection.add(mutated);
+
+            if (!added) {
+                if (mutated == null) {
+                    throw new PopulatorException("Failed to add new entry to collection as the component mutator returned null.\n" +
+                        "componentMutator: " + componentMutator.getClass());
+                }
+
+                if (newItem.equals(mutated)) {
+                    throw new PopulatorException("Failed to add new entry to collection as the component mutator did not mutate the entry.\n" +
+                        "componentMutator: " + componentMutator.getClass());
+                }
+            }
+
+            newItem = mutated;
         } while (!added);
 
         return collection;
-    }
-
-    @SuppressWarnings("unchecked")
-    private Collection<Object> ensureCollection(final Type type, final Object currentValue) {
-        Validate.isAssignableFrom(Collection.class, TypeUtils.getRawType(type, null), "Unsupported type %s", type);
-        return (Collection<Object>) currentValue;
     }
 
     @Override
@@ -84,19 +116,5 @@ public class ChangeCollectionElementsMutator implements Mutator {
     @Override
     public String toString() {
         return getClass().getSimpleName();
-    }
-
-    private Type getComponentType(Type type) {
-        return TypeUtils.getTypeArgument(type, Collection.class, COLLECTION_TYPE_VARIABLE);
-    }
-
-    private Object findNonNullElement(final Collection<Object> collection) {
-        for (Object o : collection) {
-            if (o != null) {
-                return o;
-            }
-        }
-
-        return null;
     }
 }
